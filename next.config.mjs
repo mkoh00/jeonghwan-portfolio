2dab7ee1ed246255586de3a82a29005a08afeb64
Jeong-hwan <jaohoo@daum.net>
2025-03-07 10:38:18 +0900

    feat: Next.js GitHub Pages 배포 설정 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	output: 'export', // 정적 파일을 생성하기 위한 설정
+	basePath: '/jeonghwan_portfolio', // GitHub 리포지토리명과 동일하게 설정
+	assetPrefix: '/jeonghwan_portfolio/',
+};
 
 export default nextConfig;

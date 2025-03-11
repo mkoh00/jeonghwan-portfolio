64b6447c7ebf5280d1155f86c026843dfc501948
Jeong-hwan <jaohoo@daum.net>
2025-03-11 15:12:22 +0900

    fix: nextConfig 초기화

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-	output: 'export', // 정적 파일을 생성하기 위한 설정
-	basePath: '/jeonghwan_portfolio', // GitHub 리포지토리명과 동일하게 설정
-	assetPrefix: '/jeonghwan_portfolio/',
-};
+const nextConfig = {};
 
 export default nextConfig;
